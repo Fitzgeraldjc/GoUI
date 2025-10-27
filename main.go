538bf9b1a35a4852fbe62d91e6088c5ba3307965
Jacob C. Fitzgerald <111787867+Fitzgeraldjc@users.noreply.github.com>
2025-10-27 09:41:15 -0400

    bubbleTea viewport practice

--- main.go
+++ main.go
@@ -1,44 +1,98 @@
 package main
 
 import (
-	"image/color"
 	"log"
 	"os"
 
-	"gioui.org/app"
-	"gioui.org/op"
-	"gioui.org/text"
-	"gioui.org/widget/material"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+var (
+	titleStyle = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("cyan")).
+			Padding(0, 1)
+
+	helpStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("241")).
+			Italic(true)
+
+	viewportStyle = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("62")).
+			Padding(1, 2)
+)
+
+const filename = "book.txt"
+
 func main() {
-	go func() {
-		window := new(app.Window)
-		err := run(window)
-		if err != nil {
-			log.Fatal(err)
-		}
-		os.Exit(0)
-	}()
-	app.Main()
+	p := tea.NewProgram(InitialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func run(window *app.Window) error {
-	theme := material.NewTheme()
-	var ops op.Ops
-	for {
-		switch e := window.Event().(type) {
-		case app.DestroyEvent:
-			return e.Err
-		case app.FrameEvent:
-			gtx := app.NewContext(&ops, e)
+type model struct {
+	viewport viewport.Model
+}
 
-			title := material.H1(theme, "Hello GioUI")
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
-			title.Alignment = text.Middle
-			title.Layout(gtx)
-			e.Frame(gtx.Ops)
-		}
+func ParseFile(filename string) ([]byte, error) {
+	f, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
 	}
+	return f, nil
+}
+
+func InitialModel() model {
+	content, err := ParseFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	vp := viewport.New(1, 1)
+	vp.SetContent(string(content))
+	return model{
+		viewport: vp,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c", "esc":
+			return m, tea.Quit
+		}
+
+	case tea.WindowSizeMsg:
+		headerHeight := lipgloss.Height(titleStyle.Render("Peanits Reader"))
+		helpHeight := lipgloss.Height(helpStyle.Render("↑/↓: scroll • q: quit"))
+		verticalFrameSize := 4
+		horizontalFrameSize := 6
+		m.viewport.Width = msg.Width - horizontalFrameSize
+		m.viewport.Height = msg.Height - headerHeight - helpHeight - verticalFrameSize
+	}
+
+	m.viewport, cmd = m.viewport.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	title := titleStyle.Render("Peanits Reader")
+
+	viewport := viewportStyle.Render(m.viewport.View())
+	help := helpStyle.Render("↑/↓: scroll • q: quit")
+	return lipgloss.JoinVertical(
+		lipgloss.Left,
+		title,
+		viewport,
+		help,
+	)
 }
